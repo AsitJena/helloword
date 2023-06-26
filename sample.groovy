9005a72b6cf06917994cef86b8fbca33ec65409c
AsitJena <42863668+AsitJena@users.noreply.github.com>
2023-06-26 20:56:50 +0530

    Update sample.groovy

--- sample.groovy
+++ sample.groovy
@@ -1,9 +1,1 @@
-class Example { 
-   static void main(String[] args) { 
-      // x is defined as a variable 
-      String x = "Hello";
-		
-      // The value of the variable is printed to the console 
-      println(x);
-   }
-}
+println 'Hello, World!'
